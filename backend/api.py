11f1b6a38a9086ec56986552d72f50c3d72de039
Márton Áron <aron17marton@gmail.com>
2025-12-12 19:32:13 +0100

    feat: added get, get_all endpoints, create endpoint is working fine

--- backend/api.py
+++ backend/api.py
@@ -1,8 +1,12 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 import json
+import os
+from pathlib import Path
+from utils import create_html
 
 app = Flask(__name__)
 app.config['stories_dir'] = '/home/aron/AI-n8n-website/backend/stories'
+app.config['story_ids_path'] = app.config['stories_dir'] + '/' + 'story_ids.json'
 
 @app.route('/')
 def index():
@@ -13,31 +17,70 @@ def create():
     if request.method == 'POST':
         get_body = request.json
         title = str(get_body['title']).replace(' ', '_')
+        _id = str(get_body['id'])
 
-        with open(f"{app.config['stories_dir']}/{title}.json", 'w') as file:
-            json.dump(get_body, file)
+        story_path = f"{app.config['stories_dir']}/{title}.json"
 
-        return f"'{title}' added.", 200
+        with open(story_path, 'w') as file:
+            json.dump(get_body, file, indent=4)
 
-@app.route('/delete/<id>', methods=['DELETE'])
-def delete(id):
-    if request.method == 'DELETE':
-        pass
+        ids_path = f'{app.config["stories_dir"]}/story_ids.json'
 
-@app.route('/update/<id>', methods=['PUT'])
-def update(id):
-    if request.method == 'PUT':
-        pass
+        if os.path.exists(ids_path):
+            with open(ids_path, 'r') as file:
+                story_ids = json.load(file)
+        else:
+            story_ids = {}
 
-@app.route('/get/<id>', methods=['GET'])
-def get(id):
+        story_ids[_id] = title
+
+        with open(ids_path, 'w') as file:
+            json.dump(story_ids, file, indent=4)
+
+        create_html(story_path)
+
+        return jsonify({'info': f"'{title}' added."}), 200
+    
+    return None
+
+
+
+@app.route('/get', methods=['GET'])
+def get():
     if request.method == 'GET':
-        pass
+        _id = str(request.args.get('id'))
+
+        with open(app.config['story_ids_path'], 'r') as file:
+            ids = json.load(file)
+
+            for i in ids:
+                if i == _id:
+                    with open(f"{app.config['stories_dir']}/{ids[i]}.json", 'r') as file:
+                        story = json.load(file)
+                        return story, 200
+        
+        return jsonify({'info': "No story found."}), 200
+    
+    return None
+
 
 @app.route('/get_all', methods=['GET'])
 def get_all():
     if request.method == 'GET':
-        pass
+        stories_json_dir = os.listdir(app.config['stories_dir'])
+        stories = list()
+
+        for story_file in stories_json_dir:
+            if story_file not in ['.gitignore', 'story_ids.json']:
+                path = app.config['stories_dir'] + '/' + story_file
+
+                with open(path, 'r') as file:
+                    stories.append(json.load(file))
+        
+        return jsonify(stories), 200
+    
+    return None
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)  
